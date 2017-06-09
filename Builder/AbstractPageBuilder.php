05b365fb973e29909de4b03cab6ecf972e16cc28
rajsingh8220 <raj.singh822014@gmail.com>
2017-06-09 11:55:13 -0500

    Builder

---
+++ Builder/AbstractPageBuilder.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sunny
+ * Date: 6/9/2017
+ * Time: 11:54 AM
+ */
+
+namespace builder;
+
+abstract class AbstractPageBuilder {
+    abstract function getPage();
+}
