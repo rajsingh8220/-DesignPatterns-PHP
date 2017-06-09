d2d68b51721e2020bf8c373a56eee2bebcf4d142
Sunny Singh <ssingh@netlinkvoice.com>
2017-06-09 11:20:02 -0500

    First Commit

---
+++ Factory/AbstractBookFactory.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sunny
+ * Date: 6/8/2017
+ * Time: 7:09 PM
+ */
+
+namespace factory;
+
+
+class AbstractBookFactory
+{
+
+}
---
+++ Factory/SamsBookFactory.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sunny
+ * Date: 6/8/2017
+ * Time: 7:10 PM
+ */
+
+namespace factory;
+
+
+class SamsBookFactory
+{
+
+}
