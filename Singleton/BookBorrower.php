ea01c31798abbbc2a3ce739901bca0f1dd6b1f8f
Sunny Singh <ssingh@netlinkvoice.com>
2017-06-09 11:43:35 -0500

    Singleton classes

---
+++ Singleton/BookBorrower.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sunny
+ * Date: 6/9/2017
+ * Time: 11:42 AM
+ */
+
+namespace singleton;
+
+class BookBorrower {
+    private $borrowedBook;
+    private $haveBook = FALSE;
+
+    function __construct() {
+    }
+
+    function getAuthorAndTitle() {
+        if (TRUE == $this->haveBook) {
+            return $this->borrowedBook->getAuthorAndTitle();
+        } else {
+            return "I don't have the book";
+        }
+    }
+
+    function borrowBook() {
+        $this->borrowedBook = BookSingleton::borrowBook();
+        if ($this->borrowedBook == NULL) {
+            $this->haveBook = FALSE;
+        } else {
+            $this->haveBook = TRUE;
+        }
+    }
+
+    function returnBook() {
+        $this->borrowedBook->returnBook($this->borrowedBook);
+    }
+}
---
+++ Singleton/BookSingleton.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sunny
+ * Date: 6/9/2017
+ * Time: 11:42 AM
+ */
+
+namespace singleton;
+
+class BookSingleton {
+    private $author = 'Gamma, Helm, Johnson, and Vlissides';
+    private $title  = 'Design Patterns';
+    private static $book = NULL;
+    private static $isLoanedOut = FALSE;
+
+    private function __construct() {
+    }
+
+    static function borrowBook() {
+        if (FALSE == self::$isLoanedOut) {
+            if (NULL == self::$book) {
+                self::$book = new BookSingleton();
+            }
+            self::$isLoanedOut = TRUE;
+            return self::$book;
+        } else {
+            return NULL;
+        }
+    }
+
+    function returnBook(BookSingleton $bookReturned) {
+        self::$isLoanedOut = FALSE;
+    }
+
+    function getAuthor() {return $this->author;}
+
+    function getTitle() {return $this->title;}
+
+    function getAuthorAndTitle() {
+        return $this->getTitle() . ' by ' . $this->getAuthor();
+    }
+}
